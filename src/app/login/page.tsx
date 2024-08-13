54811f2c32a8aac9b3cd1c64a295c589f5a8478c
kader009 <kadermollarasel000@gmail.com>
2024-08-13 12:30:30 +0600

    form create done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,66 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image';
+import Container from '../components/ui/Container';
+import { BsGoogle, BsGithub } from 'react-icons/bs';
+import Link from 'next/link';
 
 const LoginPage = () => {
+  const handleForm = async () =>{}
   return (
-    <div>
-      <div className="grid grid-col-2 gap-12">
+    <Container>
+      <div className="grid grid-cols-2 gap-12 my-12">
         <div>
-          <Image/>
+          <Image
+            src="/assets/images/login/login.svg"
+            height="540"
+            width="540"
+            alt="login image"
+          />
         </div>
-        <div>text form</div>
+        <div className="border-2 p-6 rounded-lg">
+          <h1 className="text-primary font-semibold text-center mb-3">Login</h1>
+          <form onSubmit={handleForm}>
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              type="text"
+              placeholder="Your email"
+              className="input input-bordered w-full mb-4"
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              type="text"
+              placeholder="Your Password"
+              className="input input-bordered w-full "
+            />
+            <br />
+            <br />
+            <button type="submit" className="btn btn-primary w-full">
+              Sign in
+            </button>
+          </form>
+          <div>
+            <h6 className="my-12 text-center">or signin with</h6>
+            <div className="flex items-center justify-center space-x-3">
+              <button className="btn flex items-center justify-center text-primary">
+                <BsGoogle />
+              </button>
+              <button className="btn flex items-center justify-center text-primary">
+                <BsGithub />
+              </button>
+            </div>
+            <h6 className="my-12 text-center">
+              New here create an account ?{' '}
+              <Link className="text-blue-700" href={'/sign-up'}>
+                Sign up
+              </Link>
+            </h6>
+          </div>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,9 +1,73 @@
+'use client'
+import Image from 'next/image';
+import Container from '../components/ui/Container';
+import { BsGoogle, BsGithub } from 'react-icons/bs';
+import Link from 'next/link';
 
 const SignupPage = () => {
+  const handleForm = async () =>{}
   return (
-    <div>
-      <h1>signup</h1>
-    </div>
+    <Container>
+      <div className="grid grid-cols-2 gap-12 my-12">
+        <div>
+          <Image
+            src="/assets/images/login/login.svg"
+            height="540"
+            width="540"
+            alt="login image"
+          />
+        </div>
+        <div className="border-2 p-6 rounded-lg">
+          <h1 className="text-primary font-semibold text-center mb-3">Signup</h1>
+          <form onSubmit={handleForm}>
+            <label htmlFor="name">Name</label>
+            <br />
+            <input
+              type="text"
+              placeholder="Your name"
+              className="input input-bordered w-full mb-4"
+            />
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              type="text"
+              placeholder="Your email"
+              className="input input-bordered w-full mb-4"
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              type="text"
+              placeholder="Your Password"
+              className="input input-bordered w-full "
+            />
+            <br />
+            <br />
+            <button type="submit" className="btn btn-primary w-full">
+              Sign in
+            </button>
+          </form>
+          <div>
+            <h6 className="my-12 text-center">or signin with</h6>
+            <div className="flex items-center justify-center space-x-3">
+              <button className="btn flex items-center justify-center text-primary">
+                <BsGoogle />
+              </button>
+              <button className="btn flex items-center justify-center text-primary">
+                <BsGithub />
+              </button>
+            </div>
+            <h6 className="my-12 text-center">
+              Already have an account ?{' '}
+              <Link className="text-blue-700" href={'/login'}>
+                Login
+              </Link>
+            </h6>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 };
 
