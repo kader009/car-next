42fa5613894aff2f00c1cb627d17dfb9a5c46562
kader009 <kadermollarasel000@gmail.com>
2024-08-15 19:00:04 +0600

    show service data

--- src/app/components/HomePage/Services.tsx
+++ src/app/components/HomePage/Services.tsx
@@ -1,8 +1,31 @@
 import Container from '../ui/Container';
 import ServiceCard from './card/ServiceCard';
-import { services } from '@/app/lib/services';
+// import { services } from '@/app/lib/services';
 
-const Services = () => {
+const getData = async () => {
+  const response = await fetch(`http://localhost:3000/services/api/get-all`);
+  const data = response.json();
+  return data;
+};
+
+interface Facility {
+  name: string;
+  details: string;
+}
+
+interface Service {
+  _id: string;
+  service_id: string;
+  title: string;
+  img: string;
+  price: string;
+  description: string;
+  facility: Facility[];
+}
+
+const Services = async() => {
+  const data = await getData();
+  console.log(data.service);
   return (
     <Container>
       <div className="text-slate-800 min-h-screen text-center mt-12">
@@ -16,12 +39,9 @@ const Services = () => {
           </p>
         </div>
         <div className="mt-12 grid grid-cols-1 lg:grid-cols-3 gap-6">
-          {
-            services.map((service) =>(
-
-              <ServiceCard service={service} key={service._id}/>
-            ))
-          }
+          {data?.service?.lenght > 0 && data?.service?.map((services :Service) => (
+            <ServiceCard service={services} key={services._id} />
+          ))}
         </div>
       </div>
     </Container>
